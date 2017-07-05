179bf3773b4259b5ca2d1b336fe52fd76af35d77
Pablo FAUST <pfaust@e1r7p4.42.fr>
2017-07-05 22:40:29 +0200

    ex02

--- ex02/ft_print_numbers.c
+++ ex02/ft_print_numbers.c
@@ -1,25 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pfaust <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/07/05 22:06:52 by pfaust            #+#    #+#             */
-/*   Updated: 2017/07/05 22:38:04 by pfaust           ###   ########.fr       */
+/*   Created: 2017/07/05 22:09:24 by pfaust            #+#    #+#             */
+/*   Updated: 2017/07/05 22:39:57 by pfaust           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_putchar(char c, int n);
 
-void	ft_print_reverse_alphabet(void)
+void	ft_print_numbers(void)
 {
 	int i;
 
-	i = 'z';
-	while (i >= 'a')
+	i = '0';
+	while (i <= '9')
 	{
 		ft_putchar(i);
-		i -= 1;
+		i = i + 1;
 	}
 }
