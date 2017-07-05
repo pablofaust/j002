1c8204c278c3dcd7fa5df0aab827cdcdc55e22b7
Pablo FAUST <pfaust@e3r2p7.42.fr>
2017-07-05 23:58:33 +0200

    ex03

--- ex03/ft_is_negative.c
+++ ex03/ft_is_negative.c
@@ -6,37 +6,16 @@
 /*   By: pfaust <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/05 22:48:17 by pfaust            #+#    #+#             */
-/*   Updated: 2017/07/05 23:05:59 by pfaust           ###   ########.fr       */
+/*   Updated: 2017/07/05 23:56:57 by pfaust           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-
-int		ft_putchar(char c)
-{
-	write(1, &c, 1);
-	return (0);
-}
+void	ft_putchar(char c);
 
 void	ft_is_negative(int n)
 {
-	int i;
-
 	if (n > 0)
-		{
-			ft_putchar(c);
-			c = 'P';
-		}
-
+		ft_putchar('P');
 	else
-		{
-			ft_puchar(c);
-			c = 'N';
-		}
-}
-
-int		main()
-{
-	ft_is_negative(int n);
-	return (0);
+		ft_putchar('N');
 }
