5d9c690169189e5831bf183b6b886ab9e87ee169
Pablo FAUST <pfaust@e3r3p1.42.fr>
2017-07-06 21:10:09 +0200

    ex06 fonctionne

--- ex06/ft_putnbr.c
+++ ex06/ft_putnbr.c
@@ -6,24 +6,38 @@
 /*   By: pfaust <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/06 15:51:26 by pfaust            #+#    #+#             */
-/*   Updated: 2017/07/06 19:17:37 by pfaust           ###   ########.fr       */
+/*   Updated: 2017/07/06 21:09:52 by pfaust           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_putchar(char c);
 
-void	ft_putnbr(int n)
+int		ft_length(int n)
 {
-	char a;
-	int exponent;
 	int length;
 
-	exponent = 10 * (length - 1);
+	length = 0;
 
 	while (n != 0)
 	{
 		length++;
-		n = n / 10;
+		n = n /10;
+	}
+	return (length);
+}
+
+void	ft_putnbr(int n)
+{
+	int exponent;
+	int length;
+
+	exponent = 1;
+	length = ft_length(n) - 1;
+
+	while(length > 0)
+	{
+		exponent = exponent * 10;
+		length --;
 	}
 
 	if (n < 0)
@@ -32,11 +46,11 @@ void	ft_putnbr(int n)
 		n = -n;
 	}
 
-	while (n > 10)
+	while (exponent >= 10)
 	{
-		a = n % 10;
-		a = a + '0';
-		ft_putchar(a);
-		n = n / (10 * exponent);
+		ft_putchar(n / exponent + 48);
+		n = n - (n / exponent * exponent);
+		exponent = exponent /10;
 	}
+	ft_putchar(n % 10 + 48);
 }
