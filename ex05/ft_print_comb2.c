5707b08b8f3729c3ffd2e584006977da790ff21c
Pablo FAUST <pfaust@e1r8p6.42.fr>
2017-07-06 22:57:26 +0200

    nouvelles tentatives ex05

--- ex05/ft_print_comb2.c
+++ ex05/ft_print_comb2.c
@@ -6,7 +6,7 @@
 /*   By: pfaust <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/06 21:51:58 by pfaust            #+#    #+#             */
-/*   Updated: 2017/07/06 22:16:17 by pfaust           ###   ########.fr       */
+/*   Updated: 2017/07/06 22:56:09 by pfaust           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,28 @@ void ft_print_comb2(void)
 	a = '0';
 	b = '0';
 	c = '0';
-	d = c + 1;
-	while (d != '0')
+	d = '1';
+	while (d <= '9')
 	{
 		ft_putchar(a);
 		ft_putchar(b);
+		ft_putchar(' ');
 		ft_putchar(c);
 		ft_putchar(d);
+		ft_putchar(',');
+		ft_putchar(' ');
 		d++;
+	}
+	c = b + 1;
+	while (c <= '9')
+	{
+		ft_putchar(a);
+		ft_putchar(b);
+		ft_putchar(' ');
+		ft_putchar(c);
+		ft_putchar('9');
+		ft_putchar(',');
+		ft_putchar(' ');
+		c++;
 	}
 }
