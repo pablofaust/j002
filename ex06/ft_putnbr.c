d9a96c40aae8c7226d538155dc8817645029028c
Pablo FAUST <pfaust@e3r3p1.42.fr>
2017-07-06 21:14:59 +0200

    ex06 norme

--- ex06/ft_putnbr.c
+++ ex06/ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: pfaust <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/06 15:51:26 by pfaust            #+#    #+#             */
-/*   Updated: 2017/07/06 21:09:52 by pfaust           ###   ########.fr       */
+/*   Updated: 2017/07/06 21:14:40 by pfaust           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,10 @@ int		ft_length(int n)
 	int length;
 
 	length = 0;
-
 	while (n != 0)
 	{
 		length++;
-		n = n /10;
+		n = n / 10;
 	}
 	return (length);
 }
@@ -33,24 +32,21 @@ void	ft_putnbr(int n)
 
 	exponent = 1;
 	length = ft_length(n) - 1;
-
-	while(length > 0)
+	while (length > 0)
 	{
 		exponent = exponent * 10;
-		length --;
+		length--;
 	}
-
 	if (n < 0)
 	{
 		ft_putchar('-');
 		n = -n;
 	}
-
 	while (exponent >= 10)
 	{
 		ft_putchar(n / exponent + 48);
 		n = n - (n / exponent * exponent);
-		exponent = exponent /10;
+		exponent = exponent / 10;
 	}
 	ft_putchar(n % 10 + 48);
 }
